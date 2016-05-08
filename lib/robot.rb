eb99e6b309f90945c8b9f2afb3981123eb386713
Callum White <callumw1991@gmail.com>
2016-05-09 00:00:09 +1000

    Test placement and turning of Robot on Table

--- lib/robot.rb
+++ lib/robot.rb
@@ -5,8 +5,26 @@ class Robot
 
   attr_reader :placement
 
+  # Mappings of [current direction] -> [next direction] for turning different ways
+  @@left_map = {:north => :west, :west => :south, :south => :east, :east => :north}
+  @@right_map = {:north => :east, :east => :south, :south => :west, :west => :north}
+  
   def initialize table
     @placement = Placement.new(Coordinate.new(0, 0), :north)
     @table = table
+  end
+
+  def place placement
+    @placement = placement if @table.on_top?(placement[:coordinate])
+  end
+  
+  def right
+    mapping = @@right_map[@placement[:dir]]
+    @placement[:dir] = mapping if mapping
+  end
+  
+  def left
+    mapping = @@left_map[@placement[:dir]]
+    @placement[:dir] = mapping if mapping
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -9,4 +9,32 @@ describe Robot do
       expect(clumsy_robot.placement).to eq Placement.new(Coordinate.new(0,0), :north)
     end
   end
+
+  describe "#place" do
+    it "updates placement" do
+      new_placement = Placement.new(Coordinate.new(2, 2), :south)
+      clumsy_robot.place new_placement
+      expect(clumsy_robot.placement).to eq new_placement
+    end
+    
+    it "doesn't update placement when invalid placement is given" do
+      invalid_placement = Placement.new(Coordinate.new(100, 100), :south)
+      clumsy_robot.place invalid_placement
+      expect(clumsy_robot.placement).not_to eq invalid_placement
+    end
+  end
+
+  describe "#right" do
+    it "updates direction upon right turn" do
+      expect(clumsy_robot.right).to eq :east
+      expect(clumsy_robot.right).to eq :south
+    end
+  end
+
+  describe "#left" do
+    it "updates direction upon left turn" do
+      expect(clumsy_robot.left).to eq :west
+      expect(clumsy_robot.left).to eq :south
+    end
+  end
 end
